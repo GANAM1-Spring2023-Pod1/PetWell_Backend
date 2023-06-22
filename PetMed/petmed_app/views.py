141aa587d06725b403fac6395e2d19a177f63de6
individual-ism <individual-ism-code@proton.me>
2023-06-21 23:20:13 -0400

    Establishes serializers and views for user registration and sign in

--- PetMed/petmed_app/views.py
+++ PetMed/petmed_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from knox.models import AuthToken
+
+from .serializers import UserProfileSerializer, PetSerializer, VaccineSerializer, MedicationSerializer
+from .models import UserProfile, Pet, Vaccine, Medication
 
 # Create your views here.
+
+# User Registration View Set
