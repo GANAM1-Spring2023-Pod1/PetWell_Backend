d52aceec929f86d790fec5b789395519962f13ee
individual-ism <individual-ism-code@proton.me>
2023-06-26 19:32:39 -0400

    Corrects debug mode

--- petwell/settings.py
+++ petwell/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sq84ki#o&um@a0z5g7)==l4ik@*hf%1v+$ja$7yx^+w_p2zh0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
