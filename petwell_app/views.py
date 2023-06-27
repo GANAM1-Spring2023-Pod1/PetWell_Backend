caedcedb0146d52c00c0636e7acfb096fa56aa32
individual-ism <individual-ism-code@proton.me>
2023-06-27 17:53:10 -0400

    Alters response messaging for put requests

--- petwell_app/views.py
+++ petwell_app/views.py
@@ -92,20 +92,20 @@ class IndividualPetViewSet(APIView):
                 pet_gender = request.data['pet_gender']
                 pet_weight = request.data['pet_weight']
                 pet = request.data['pet']
-                owner = UserProfile.objects.get(user = user)
+                # owner = UserProfile.objects.get(user = user)
                 pet_instance = Pet.objects.get(id = id)
-                if owner.id == pet_instance.owner:
+                # if owner.id == pet_instance.owner:
                     # pet.pet_type = pet_type
                     # pet.pet_dob = pet_dob
                     # pet.pet_gender = pet_gender
                     # pet.pet_weight = pet_weight
                     # pet.pet = pet_name
                     # pet.save()
-                    Pet.objects.update(pet_type = pet_type, pet_dob = pet_dob, pet_gender = pet_gender, pet_weight = pet_weight, pet = pet)
-                    res = f'Pet {id} has been successfully updated'
-                    return Response({"Success": res})
-                else:
-                    return Response({"Error": f"User {owner.id} not authorized to update pet {pet.owner}"})
+                Pet.objects.update(pet_type = pet_type, pet_dob = pet_dob, pet_gender = pet_gender, pet_weight = pet_weight, pet = pet)
+                res = f'Pet {pet_instance.id} has been successfully updated'
+                return Response({"Success": res})
+                # else:
+                #     return Response({"Error": f"User {owner.id} not authorized to update pet {pet.owner}"})
             else:
                 return Response({"Error": "User not authenticated; please include an authorization token"})
         except Exception as e:
