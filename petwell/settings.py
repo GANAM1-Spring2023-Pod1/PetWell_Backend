f663076005ce3d4586f472fd045d13845c9843e1
individual-ism <individual-ism-code@proton.me>
2023-06-27 14:38:19 -0400

    Includes CORS_ALLOW_METHODS in settings.py

--- petwell/settings.py
+++ petwell/settings.py
@@ -33,6 +33,16 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:8000', 'http://127.0.0.1:3000', 'http://127.0.0.1:8000']
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT'
+]
+
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
